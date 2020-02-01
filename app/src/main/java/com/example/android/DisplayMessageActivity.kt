583e2f52abd527ddb2a1d43121e41384025582ed
holy <holy1017@naver.com>
2020-02-01 22:46:40 +0900

    아이콘 사라졌다 나타났다

--- app/src/main/java/com/example/android/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/android/DisplayMessageActivity.kt
@@ -1,11 +1,16 @@
 package com.example.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_display_message.*
+
 
 class DisplayMessageActivity : AppCompatActivity() {
+
+    //var rSwitch=0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,17 @@ class DisplayMessageActivity : AppCompatActivity() {
             text = message
         }
 
+        onClickSwitch( findViewById<TextView>(R.id.switch1))
+    }
+
+    fun onClickSwitch(v:View) {
+        when (switch1.isChecked) {
+            true -> {
+                imageView2.visibility = View.VISIBLE
+            }
+            false -> {
+                imageView2.visibility = View.INVISIBLE
+            }
+        }
     }
 }
